0a710be686422badc0649759df75a739c83f85e8
DeadMoon0 <94380754+DeadMoon0@users.noreply.github.com>
2026-09-05 01:03:40 +0200

    Use custom contract resolver to prevent converter recursion

--- nem.Common/Attributes/JsonHeaderConverter.cs
+++ nem.Common/Attributes/JsonHeaderConverter.cs
@@ -1,7 +1,7 @@
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Concurrent;
-using System.Threading;
 using nem.Common.Models;
 
 namespace nem.Common.Attributes;
@@ -9,38 +9,27 @@ namespace nem.Common.Attributes;
 public class JsonHeaderConverter : JsonConverter
 {
     private static readonly ConcurrentDictionary<Type, string> _headers = new();
-    private static readonly ThreadLocal<int?> _depth = new(() => null);
+
+    static readonly JsonSerializerSettings _innerSettings = new()
+    {
+        Formatting = Formatting.Indented,
+        ContractResolver = new JsonHeaderContractResolver()
+    };
 
     public override bool CanConvert(Type objectType) => typeof(NemConfig).IsAssignableFrom(objectType);
 
     public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
     {
-        _depth.Value = (_depth.Value ?? 0) + 1;
-        try
+        var header = GetHeader(value.GetType());
+        if (!string.IsNullOrEmpty(header))
         {
-            if (_depth.Value > 1)
-            {
-                serializer.Serialize(writer, value);
-                return;
-            }
-            
-            var header = GetHeader(value.GetType());
-            if (!string.IsNullOrEmpty(header))
-            {
-                writer.WriteRaw(header + "\n  ");
-            }
-            
-            var settings = new JsonSerializerSettings { Formatting = Formatting.Indented };
-            
-            var tempWriter = new System.IO.StringWriter();
-            var tempSerializer = JsonSerializer.Create(settings);
-            tempSerializer.Serialize(tempWriter, value);
-            writer.WriteRaw(tempWriter.ToString());
+            writer.WriteRaw(header + "\n  ");
         }
-        finally
-        {
-            _depth.Value = (_depth.Value ?? 0) - 1;
-        }
+        
+        var tempWriter = new System.IO.StringWriter();
+        var tempSerializer = JsonSerializer.Create(_innerSettings);
+        tempSerializer.Serialize(tempWriter, value);
+        writer.WriteRaw(tempWriter.ToString());
     }
 
     public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
@@ -55,5 +44,15 @@ public class JsonHeaderConverter : JsonConverter
                 .FirstOrDefault();
             return attr?.Header ?? string.Empty;
         });
+    }
+}
+
+public class JsonHeaderContractResolver : DefaultContractResolver
+{
+    protected override JsonConverter ResolveContractConverter(Type objectType)
+    {
+        if (typeof(NemConfig).IsAssignableFrom(objectType))
+            return null;
+        return base.ResolveContractConverter(objectType);
     }
 }
